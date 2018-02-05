56d76bb42262ec4458fe630cf5f1e16567c0b993
luong minh trung <luongminhtrung1691994@gmail.com>
2018-02-05 21:21:31 +0700

    test component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,6 @@
 <template>
-  <div>asdsa</div>
+  <div>1d1 d1dd1</div>
 </template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
