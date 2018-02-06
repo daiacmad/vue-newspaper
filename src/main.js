9f1565dcb72f7cb4d3285f3d8b0270ebd7bbc81a
trung.luong@apps-cyclone.com <trung.luong@apps-cyclone.com>
2018-02-06 14:23:03 +0700

    UI login page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import "./lib/adminlte/css/bootstrap.min.css"
 import "./lib/adminlte/css/AdminLTE.min.css"
 
 Vue.config.productionTip = false
